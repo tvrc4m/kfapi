082f38c31e8ceddbb0fefee31998ae657c90b182
sinchie <sinchiezh@gmail.com>
2018-03-07 18:17:16 +0800

    http code

--- app/helps.php
+++ app/helps.php
@@ -33,16 +33,15 @@ if (! function_exists('api_error')) {
      * 失败消息
      * @param string $message
      * @param int $error_code
-     * @param int $http_code
      * @param array $header
      * @return \Illuminate\Http\JsonResponse
      */
-    function api_error(string $message = 'error', int $error_code = 1, int $http_code = 400, array $header = []) {
+    function api_error(string $message = 'error', int $error_code = 1, array $header = []) {
         $resp = [
             'error_no' => $error_code,
             'error_message' => $message,
         ];
-        return response()->json($resp, $http_code, $header);
+        return response()->json($resp, 200, $header);
     }
 }
 
