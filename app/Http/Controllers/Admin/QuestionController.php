0a848aaf1e6c4b69d9323067973fa8db9175d817
sinchie <sinchiezh@gmail.com>
2018-03-14 10:41:17 +0800

    问题model封装

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -58,44 +58,12 @@ class QuestionController extends Controller
             'options.*.keyword.array' => '选项关键词必须是数组',
         ]);
 
-        // 问题基础信息
-        $baseData = $request->except('options');
-        // 选项信息
-        $options = $request->only('options');
-        // 开启事务
-        DB::beginTransaction();
-        // 保存基础信息
-        $question = Question::create($baseData);
-        if (!$question) {
-            DB::rollBack();
-            return api_error();
-        }
-        // 保存选项信息
-        foreach ($options['options'] as $option) {
-            $questionOption = QuestionOption::create([
-                'question_id' => $question->id,
-                'options' => $option['name'],
-                'weight' => intval($option['weight'] ?? 0)
-            ]);
-            if (!$questionOption) {
-                DB::rollBack();
-                return api_error();
-            }
-            foreach ($option['keyword'] as $keyword_id) {
-                // 保存关键词信息
-                $keyword = QuestionOptionKeyword::create([
-                    'question_option_id' => $questionOption->id,
-                    'keyword_id' => $keyword_id,
-                ]);
-                if (!$keyword) {
-                    DB::rollBack();
-                    return api_error();
-                }
-            }
+        $question = new Question();
+        if ($question->saveQuestion($request)) {
+            return api_success();
         }
 
-        DB::commit();
-        return api_success();
+        return api_error();
     }
 
     /**
@@ -132,59 +100,12 @@ class QuestionController extends Controller
             'options.*.keyword.array' => '选项关键词必须是数组',
         ]);
 
-        // 问题基础信息
-        $baseData = $request->except('options');
-        // 选项信息
-        $options = $request->only('options');
-        // 开启事务
-        DB::beginTransaction();
-        // 保存基础信息
-        $question = Question::where('id', $id)->firstOrFail();
-        if (!$question->update($baseData)) {
-            DB::rollBack();
-            return api_error();
-        }
-        // 删除原来的选项信息
-        $ops = QuestionOption::where('question_id', $question->id)->with('keyword')->get();
-        foreach ($ops as $op) {
-            $del = $op->keyword()->detach();
-            if (!$del) {
-                DB::rollBack();
-                return api_error();
-            }
-        }
-        $del = QuestionOption::where('question_id', $question->id)->delete();
-        if (!$del) {
-            DB::rollBack();
-            return api_error();
-        }
-        // 保存选项信息
-        foreach ($options['options'] as $option) {
-            // 保存新的选项信息
-            $questionOption = QuestionOption::create([
-                'question_id' => $question->id,
-                'options' => $option['name'],
-                'weight' => intval($option['weight'] ?? 0)
-            ]);
-            if (!$questionOption) {
-                DB::rollBack();
-                return api_error();
-            }
-            foreach ($option['keyword'] as $keyword_id) {
-                // 保存关键词信息
-                $keyword = QuestionOptionKeyword::create([
-                    'question_option_id' => $questionOption->id,
-                    'keyword_id' => $keyword_id,
-                ]);
-                if (!$keyword) {
-                    DB::rollBack();
-                    return api_error();
-                }
-            }
+        $question = new Question();
+        if ($question->saveQuestion($request, $id)) {
+            return api_success();
         }
 
-        DB::commit();
-        return api_success();
+        return api_error();
     }
 
     /**
@@ -195,27 +116,12 @@ class QuestionController extends Controller
      */
     public function deleteQuestion($id)
     {
-        DB::beginTransaction();
-        $del = Question::destroy($id);
-        if (!$del) {
-            DB::rollBack();
-            return api_error();
+        $model = new Question();
+        if ($model->deleteQuestion($id)) {
+            return api_success();
         }
-        $ops = QuestionOption::where('question_id', $id)->with('keyword')->get();
-        foreach ($ops as $op) {
-            $del = $op->keyword()->detach();
-            if (!$del) {
-                DB::rollBack();
-                return api_error();
-            }
-        }
-        $del = QuestionOption::where('question_id', $id)->delete();
-        if (!$del) {
-            DB::rollBack();
-            return api_error();
-        }
-        DB::commit();
-        return api_success();
+
+        return api_error();
     }
 
     /**
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,6 +3,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Question extends Model
 {
@@ -32,5 +34,109 @@ class Question extends Model
     public function questionOption()
     {
         return $this->hasMany(\App\Models\QuestionOption::class, 'question_id','id');
+    }
+
+    /**
+     * 保存问题
+     * @param Request $request
+     * @param int $id
+     * @return bool|mixed
+     * @throws \Exception
+     */
+    public function saveQuestion(Request $request, $id = 0)
+    {
+        // 问题基础信息
+        $baseData = $request->except('options');
+        // 选项信息
+        $options = $request->only('options');
+
+        // 开启事务
+        DB::beginTransaction();
+        if (empty($id)) { // 新增
+            $question = $this->create($baseData);
+            if (!$question) {
+                DB::rollBack();
+                return false;
+            }
+        } else { // 更新
+            $question = $this->where('id', $id)->firstOrFail();
+            if (!$question->update($baseData)) {
+                DB::rollBack();
+                return false;
+            }
+            // 删除原来的关键词信息
+            $ops = QuestionOption::where('question_id', $question->id)->with('keyword')->get();
+            foreach ($ops as $op) {
+                $del = $op->keyword()->detach();
+                if (!$del) {
+                    DB::rollBack();
+                    return false;
+                }
+            }
+            // 删除原来的选项信息
+            $del = QuestionOption::where('question_id', $question->id)->delete();
+            if (!$del) {
+                DB::rollBack();
+                return false;
+            }
+        }
+
+        // 保存选项信息
+        foreach ($options['options'] as $option) {
+            $questionOption = QuestionOption::create([
+                'question_id' => $question->id,
+                'options' => $option['name'],
+                'weight' => intval($option['weight'] ?? 0)
+            ]);
+            if (!$questionOption) {
+                DB::rollBack();
+                return false;
+            }
+            foreach ($option['keyword'] as $keyword_id) {
+                // 保存关键词信息
+                $keyword = QuestionOptionKeyword::create([
+                    'question_option_id' => $questionOption->id,
+                    'keyword_id' => $keyword_id,
+                ]);
+                if (!$keyword) {
+                    DB::rollBack();
+                    return false;
+                }
+            }
+        }
+
+        DB::commit();
+        return $question->id;
+    }
+
+    /**
+     * 删除问题
+     * @param $id
+     * @return bool
+     * @throws \Exception
+     */
+    public function deleteQuestion($id)
+    {
+        DB::beginTransaction();
+        $del = Question::destroy($id);
+        if (!$del) {
+            DB::rollBack();
+            return false;
+        }
+        $ops = QuestionOption::where('question_id', $id)->with('keyword')->get();
+        foreach ($ops as $op) {
+            $del = $op->keyword()->detach();
+            if (!$del) {
+                DB::rollBack();
+                return false;
+            }
+        }
+        $del = QuestionOption::where('question_id', $id)->delete();
+        if (!$del) {
+            DB::rollBack();
+            return false;
+        }
+        DB::commit();
+        return true;
     }
 }
