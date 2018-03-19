eec0edf202bf6fcc23fd95637985b42505f587e8
wuchao <705013520@qq.com>
2018-03-19 15:00:26 +0800

    调试大家在测接口

--- app/Http/Controllers/V1/TopicController.php
+++ app/Http/Controllers/V1/TopicController.php
@@ -22,7 +22,7 @@ class TopicController extends Controller
     public function getAllTopic(Request $request)
     {
         //dd($request->header());
-        file_put_contents('/tmp/topic.log',$request->header());
+        file_put_contents('/tmp/topic.log',$request->header('device'));
         $perpage = $request->input('per_page');
 
         $topics = DB::table('topics')
