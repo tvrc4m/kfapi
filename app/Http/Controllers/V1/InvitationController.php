ca6eb34d9acf2000d7f5355a34131d586907b7e8
wuchao <705013520@qq.com>
2018-03-20 16:49:31 +0800

    专家详情bug

--- app/Http/Controllers/V1/InvitationController.php
+++ app/Http/Controllers/V1/InvitationController.php
@@ -22,20 +22,23 @@ class InvitationController extends Controller
     //用户邀请
     public function addInvitation(Request $request)
     {
+        $user_id = \Auth::user()['id'];
+        //dd($user_id);
         $this->validate($request, [
-            'user_id' => 'required|numeric',
             'topic_id' => 'required|numeric',
             'expert_id' => 'required|numeric',
         ],[
-            'user_id.required' => '用户ID不能为空',
-            'user_id.numeric' => '用户ID不合法',
             'topic_id.required' => '问题id不能为空',
             'topic_id.numeric' => '问题id不合法',
             'expert_id.required' => '专家id不能为空',
             'expert_id.numeric' => '专家id不合法',
         ]);
-
-        $result = Invitation::create($request->all());
+        $data = array(
+            'user_id'=>$user_id,
+            'topic_id'=>$request->input('topic_id'),
+            'expert_id'=>$request->input('expert_id'),
+        );
+        $result = Invitation::create($data);
         if ($result) {
             return api_success();
         }
