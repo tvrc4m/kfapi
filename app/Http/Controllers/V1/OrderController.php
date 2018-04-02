5ec90a9468a8491185da3c03cc42292e13585864
wuchao <705013520@qq.com>
2018-04-02 15:11:29 +0800

    订单

--- app/Http/Controllers/V1/OrderController.php
+++ app/Http/Controllers/V1/OrderController.php
@@ -24,7 +24,7 @@ class OrderController extends Controller
     public function addOrder(Request $request)
     {
         //dd($request);
-
+        file_put_contents('/tmp/order.log',$request->input('service_id'));
         $this->validate($request, [
             'service_id' => 'required|numeric',
             'expert_id' => 'required|numeric',
@@ -48,7 +48,6 @@ class OrderController extends Controller
         $data=$request->except('user_name','phone');
         $userId = \Auth::user()['id'];
         $data['user_id'] = $userId;
-        file_put_contents('/tmp/order.log',$data).'\n';
         dd($data);
         // 开启事务
         DB::beginTransaction();
